4ffa29482ef27b199d905454a53581661f776064
Galdhaym <zloy.goblin.ork@gmail.com>
2022-11-07 18:56:16 +0200

    Added User schema

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: [String],
+        enum: ["user", "admin"],
+        default: ["user"],
+    },
+});
+
+var User = mongoose.model("User", UserSchema);
+
+module.exports = User;
