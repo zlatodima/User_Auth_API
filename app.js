1cba74e6eccb0311bfa8168228b09c59bea19dc2
Galdhaym <zloy.goblin.ork@gmail.com>
2022-11-08 15:21:05 +0200

    Added auth routes

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ var app = express();
 var dotenv = require("dotenv");
 dotenv.config();
 
+app.use(express.json());
+
 const DBConnect = require("./DBConnect");
 DBConnect();
+
+var authRoutes = require("./routes/auth_routes");
+app.use("/api", authRoutes);
 
 const port = process.env.PORT;
 app.listen(port);
---
+++ routes/auth_routes.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+
+router.post("/register", function(req, res){
+
+});
+
+router.post("/login", function(req, res){
+    
+});
+
+module.exports = router;
