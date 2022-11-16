44b65531ed75d40421c25010e6bd81b308c80bf1
Galdhaym <zloy.goblin.ork@gmail.com>
2022-11-16 12:27:13 +0200

    Added refresh token verification

---
+++ middlewares/verifyRefreshToken.js
@@ -0,0 +1,22 @@
+var jwt = require("jsonwebtoken");
+const User = require("../models/User");
+var verifyRefreshToken = async function(refreshToken){
+    try{
+        var payloadRefreshToken = await jwt.verify(refreshToken, process.env.SECRET);
+        if(!payloadRefreshToken){
+            throw new Error("Invalid Token!");
+        }
+
+        var dbRefreshToken = await User.collection.findOne({userId: payload._id});
+
+        if(!dbRefreshToken){
+            throw new Error("Invalid Token!");
+        }
+
+        return payload;
+    }
+    catch(err){
+        throw new Error("Invalid Token!");
+    }
+
+}
